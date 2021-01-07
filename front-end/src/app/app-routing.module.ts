95dfeee1b319fd3136b17804b0af38fd9125c796
Natanael <NatanaelUF17@gmail.com>
2021-01-07 11:03:27 -0400

    Se crearon varios componentes y se colocaron algunas reglas de ruteo 🕸

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrearActorComponent } from './actores/crear-actor/crear-actor.component';
+import { IndiceActoresComponent } from './actores/indice-actores/indice-actores.component';
+import { CrearCineComponent } from './cines/crear-cine/crear-cine.component';
+import { IndiceCinesComponent } from './cines/indice-cines/indice-cines.component';
+import { CrearGeneroComponent } from './generos/crear-genero/crear-genero.component';
 import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CrearPeliculaComponent } from './peliculas/crear-pelicula/crear-pelicula.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'generos', component: IndiceGenerosComponent}
+  //Rutas para los componentes de generos
+  { path: 'generos', component: IndiceGenerosComponent },
+  { path: 'generos/crear', component: CrearGeneroComponent },
+  //Rutas para los componentes de actores
+  { path: 'actores', component: IndiceActoresComponent },
+  { path: 'actores/crear', component: CrearActorComponent },
+  //Rutas para los componentes de cines
+  { path: 'cines', component: IndiceCinesComponent },
+  { path: 'cines/crear', component: CrearCineComponent },
+  //Rutas para los componentes de peliculas
+  { path: 'peliculas/crear', component: CrearPeliculaComponent }
 ];
 
 @NgModule({
